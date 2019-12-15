95672296dffb2c3adc080a3d21e1b11987c0772e
Juan Casas <imhouses@ciencias.unam.mx>
2019-12-15 14:56:34 -0600

    Added: Transformers object to transform models between layers.

---
+++ app/src/main/java/io/jcasas/weatherdagger2example/model/transformation/Transformers.kt
@@ -0,0 +1,22 @@
+package io.jcasas.weatherdagger2example.model.transformation
+
+import io.jcasas.weatherdagger2example.domain.weather.WeatherEntity
+import io.jcasas.weatherdagger2example.model.Weather
+
+/**
+ * Object to perform a transformation between Business Layer objects and App specific models.
+ */
+object Transformers {
+
+    fun fromDomainWeather(weather: WeatherEntity): Weather = Weather(
+            weatherId = weather.weatherId,
+            status = weather.status,
+            statusDescription = weather.statusDescription,
+            temperature = weather.temperature.toInt(),
+            maxTemperature = weather.maxTemperature,
+            minTemperature = weather.minTemperature,
+            units = weather.units,
+            locationName = weather.locationName,
+            coordinates = weather.coordinates
+    )
+}
