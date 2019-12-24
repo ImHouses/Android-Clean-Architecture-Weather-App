44db3d3cf60092ab2ebef86e36bb8145540c8f37
Juan Casas <imhouses@ciencias.unam.mx>
2019-12-24 00:18:04 -0600

    Feature: Location cache.

--- app/src/main/java/io/jcasas/weatherdagger2example/framework/AppLocationDataSource.kt
+++ app/src/main/java/io/jcasas/weatherdagger2example/framework/AppLocationDataSource.kt
@@ -7,14 +7,23 @@ import io.jcasas.weatherdagger2example.data.exceptions.LocationNullException
 import io.jcasas.weatherdagger2example.data.location.LocationDataSource
 import io.jcasas.weatherdagger2example.domain.Coordinates
 import kotlinx.coroutines.tasks.await
+import org.joda.time.DateTime
 
 class AppLocationDataSource(private val context: Context) : LocationDataSource {
 
     private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var lastCoordinates: Coordinates
+    private var lastUpdateTime: DateTime = DateTime.now()
+    private val threshold = 3 * 600_000
 
     override suspend fun getCurrent(): Coordinates {
+        val timeDifference = DateTime.now().millis - lastUpdateTime.millis
+        if (timeDifference < threshold && this::lastCoordinates.isInitialized) {
+            return lastCoordinates
+        }
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(context)
         val location = fusedLocationClient.lastLocation.await() ?: throw LocationNullException()
-        return Coordinates(lon = location.longitude, lat = location.latitude)
+        lastCoordinates = Coordinates(lon = location.longitude, lat = location.latitude)
+        return lastCoordinates
     }
 }
