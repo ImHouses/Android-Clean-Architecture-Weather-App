30deac67b4246b459f71f187069cf929a5a7a794
Juan Casas <imhouses@ciencias.unam.mx>
2019-12-27 17:04:45 -0600

    Changed: Updated copyright.

--- app/src/main/java/io/jcasas/weatherdagger2example/di/component/WeatherAppComponent.kt
+++ app/src/main/java/io/jcasas/weatherdagger2example/di/component/WeatherAppComponent.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017, Juan Casas
+ * Copyright 2019, Juan Casas
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
