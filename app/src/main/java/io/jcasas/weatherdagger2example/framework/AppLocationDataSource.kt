53ad64bf114f088ca5e49adac540202bed36699c
Juan Casas <imhouses@ciencias.unam.mx>
2019-12-27 16:19:01 -0600

    Changed: App location data source saves the last location update time.

--- app/src/main/java/io/jcasas/weatherdagger2example/framework/AppLocationDataSource.kt
+++ app/src/main/java/io/jcasas/weatherdagger2example/framework/AppLocationDataSource.kt
@@ -1,30 +1,41 @@
 package io.jcasas.weatherdagger2example.framework
 
 import android.content.Context
+import android.content.SharedPreferences
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import io.jcasas.weatherdagger2example.data.exceptions.LocationNullException
 import io.jcasas.weatherdagger2example.data.location.LocationDataSource
 import io.jcasas.weatherdagger2example.domain.Coordinates
+import io.jcasas.weatherdagger2example.util.Constants
 import kotlinx.coroutines.tasks.await
-import org.joda.time.DateTime
+import javax.inject.Inject
+import javax.inject.Named
 
-// TODO: Persistence of last update time.
-class AppLocationDataSource(private val context: Context) : LocationDataSource {
+class AppLocationDataSource @Inject constructor(
+        private val context: Context,
+        @Named("internal_config")
+        private val sharedPreferences: SharedPreferences
+) : LocationDataSource {
 
     private lateinit var fusedLocationClient: FusedLocationProviderClient
     private lateinit var lastCoordinates: Coordinates
-    private var lastUpdateTime: DateTime = DateTime.now()
+    private var lastUpdateTime: Long =
+            sharedPreferences.getLong(Constants.Keys.LOCATION_LAST_UPDATE, System.currentTimeMillis())
     private val threshold = 3 * 600_000
 
     override suspend fun getCurrent(): Coordinates {
-        val timeDifference = DateTime.now().millis - lastUpdateTime.millis
+        val timeDifference = System.currentTimeMillis() - lastUpdateTime
+        println(System.currentTimeMillis())
         if (timeDifference < threshold && this::lastCoordinates.isInitialized) {
             return lastCoordinates
         }
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(context)
         val location = fusedLocationClient.lastLocation.await() ?: throw LocationNullException()
         lastCoordinates = Coordinates(lon = location.longitude, lat = location.latitude)
+        sharedPreferences.edit().apply {
+            putLong(Constants.Keys.LOCATION_LAST_UPDATE, System.currentTimeMillis())
+        }.apply()
         return lastCoordinates
     }
 }
