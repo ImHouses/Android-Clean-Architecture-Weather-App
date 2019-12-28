20ea7e700f81bf673ab2310d4cba581adc25b342
Juan Casas <imhouses@ciencias.unam.mx>
2019-12-27 22:27:01 -0600

    Changed: Room entity rollback.

--- app/src/main/java/io/jcasas/weatherdagger2example/model/Weather.kt
+++ app/src/main/java/io/jcasas/weatherdagger2example/model/Weather.kt
@@ -1,26 +1,16 @@
 package io.jcasas.weatherdagger2example.model
 
-import androidx.room.ColumnInfo
-import androidx.room.Entity
-import androidx.room.PrimaryKey
 import io.jcasas.weatherdagger2example.domain.Coordinates
 import io.jcasas.weatherdagger2example.domain.Units
 
-@Entity(tableName = "weathers")
 data class Weather(
-        @ColumnInfo(name = "weather_id")
         val weatherId: Int,
         val status: String,
-        @ColumnInfo(name = "status_description")
         val statusDescription: String,
         val temperature: Int,
-        @ColumnInfo(name = "min_temperature")
         val minTemperature: Double,
-        @ColumnInfo(name = "max_temperature")
         val maxTemperature: Double,
         val units: Units,
-        @PrimaryKey
-        @ColumnInfo(name = "location_name")
         val locationName: String,
         val coordinates: Coordinates
 )
