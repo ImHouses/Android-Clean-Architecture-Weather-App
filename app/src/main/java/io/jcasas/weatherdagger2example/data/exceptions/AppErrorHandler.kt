2c3b7578ab47213fb85bc858fc3388f704c96868
Juan Casas <imhouses@ciencias.unam.mx>
2020-01-10 15:18:25 -0600

    Changed: Added more exception handling.

--- app/src/main/java/io/jcasas/weatherdagger2example/data/exceptions/AppErrorHandler.kt
+++ app/src/main/java/io/jcasas/weatherdagger2example/data/exceptions/AppErrorHandler.kt
@@ -3,6 +3,8 @@ package io.jcasas.weatherdagger2example.data.exceptions
 import io.jcasas.weatherdagger2example.domain.ErrorEntity
 import io.jcasas.weatherdagger2example.domain.ErrorHandler
 import retrofit2.HttpException
+import java.io.IOException
+import java.net.SocketTimeoutException
 
 class AppErrorHandler : ErrorHandler {
 
@@ -10,6 +12,8 @@ class AppErrorHandler : ErrorHandler {
         is ConnectivityException -> ErrorEntity.Network(throwable)
         is LocationNullException -> ErrorEntity.LocationError(throwable)
         is HttpException -> ErrorEntity.Network(throwable)
+        is SocketTimeoutException -> ErrorEntity.ServiceUnavailable(throwable)
+        is IOException -> ErrorEntity.Network(throwable)
         is IllegalArgumentException -> ErrorEntity.ServiceUnavailable(throwable)
         else -> ErrorEntity.Unknown(throwable)
     }
