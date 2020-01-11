0c4d6a9284819eba2e00e32845d37a11b48da3d0
Juan Casas <imhouses@ciencias.unam.mx>
2020-01-10 23:45:59 -0600

    Changed: Replaced implementations with abstractions on GetOneWeekForecast interactor.

--- app/src/main/java/io/jcasas/weatherdagger2example/interactors/GetOneWeekForecast.kt
+++ app/src/main/java/io/jcasas/weatherdagger2example/interactors/GetOneWeekForecast.kt
@@ -1,7 +1,7 @@
 package io.jcasas.weatherdagger2example.interactors
 
-import io.jcasas.weatherdagger2example.data.location.AppLocationRepository
-import io.jcasas.weatherdagger2example.data.weather.AppWeatherRepository
+import io.jcasas.weatherdagger2example.data.location.LocationRepository
+import io.jcasas.weatherdagger2example.data.weather.WeatherRepository
 import io.jcasas.weatherdagger2example.domain.ErrorHandler
 import io.jcasas.weatherdagger2example.domain.forecast.ForecastEntity
 import io.jcasas.weatherdagger2example.util.Resource
@@ -9,8 +9,8 @@ import io.jcasas.weatherdagger2example.util.tryOrHandle
 import javax.inject.Inject
 
 class GetOneWeekForecast @Inject constructor(
-        private val weatherRepository: AppWeatherRepository,
-        private val locationRepository: AppLocationRepository,
+        private val weatherRepository: WeatherRepository,
+        private val locationRepository: LocationRepository,
         private val errorHandler: ErrorHandler
 ) {
 
