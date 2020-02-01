e5a8b8f52a78cf39753450331bf80d581ea74081
Juan Casas <imhouses@ciencias.unam.mx>
2020-02-01 16:19:12 -0600

    Changed: Initialized testing for AppWeatherRepository.

--- app/src/main/java/io/jcasas/weatherdagger2example/data/weather/AppWeatherRepository.kt
+++ app/src/main/java/io/jcasas/weatherdagger2example/data/weather/AppWeatherRepository.kt
@@ -1,7 +1,9 @@
 package io.jcasas.weatherdagger2example.data.weather
 
 import io.jcasas.weatherdagger2example.data.config.ConfigurationDataSource
+import io.jcasas.weatherdagger2example.data.exceptions.ConnectivityException
 import io.jcasas.weatherdagger2example.domain.Coordinates
+import io.jcasas.weatherdagger2example.domain.config.NetworkStatus
 import io.jcasas.weatherdagger2example.domain.forecast.ForecastEntity
 import io.jcasas.weatherdagger2example.domain.weather.WeatherEntity
 import javax.inject.Inject
@@ -13,12 +15,20 @@ class AppWeatherRepository @Inject constructor(
 
     /* 3 hours. */
     private val threshold: Long = 3 * 60 * 60 * 1000
+        get() = field
 
-    override suspend fun getCurrentWeather(coordinates: Coordinates): WeatherEntity =
-            dataSource.getCurrentWeatherFromService(
-                    coordinates,
-                    configurationDataSource.getConfiguration().defaultUnits
-            )
+    override suspend fun getCurrentWeather(coordinates: Coordinates): WeatherEntity {
+        val networkStatus = configurationDataSource.getNetworkStatus()
+        val savedUnits = configurationDataSource.getConfiguration().defaultUnits
+        val savedWeather = dataSource.getCurrentWeatherFromLocal(savedUnits)
+        if (savedWeather == null && networkStatus == NetworkStatus.NOT_CONNECTED) {
+            throw ConnectivityException()
+        }
+        return dataSource.getCurrentWeatherFromService(
+                coordinates,
+                configurationDataSource.getConfiguration().defaultUnits
+        )
+    }
 
     override suspend fun get5DayCurrentForecast(coordinates: Coordinates): List<ForecastEntity> =
             dataSource.getCurrent5DayForecastFromService(
--- app/src/test/java/io/jcasas/weatherdagger2example/AppWeatherRepositoryTest.kt
+++ app/src/test/java/io/jcasas/weatherdagger2example/AppWeatherRepositoryTest.kt
@@ -1,16 +1,109 @@
 package io.jcasas.weatherdagger2example
 
+import com.nhaarman.mockitokotlin2.*
+import com.nhaarman.mockitokotlin2.internal.createInstance
+import io.jcasas.weatherdagger2example.data.config.ConfigurationDataSource
+import io.jcasas.weatherdagger2example.data.exceptions.ConnectivityException
+import io.jcasas.weatherdagger2example.data.weather.AppWeatherRepository
+import io.jcasas.weatherdagger2example.data.weather.WeatherDataSource
+import io.jcasas.weatherdagger2example.domain.Coordinates
+import io.jcasas.weatherdagger2example.domain.Units
+import io.jcasas.weatherdagger2example.domain.config.Configuration
+import io.jcasas.weatherdagger2example.domain.config.NetworkStatus
+import io.jcasas.weatherdagger2example.domain.forecast.ForecastEntity
+import io.jcasas.weatherdagger2example.domain.weather.WeatherEntity
+import kotlinx.coroutines.runBlocking
+import org.joda.time.DateTime
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.Before
+import kotlin.reflect.KClass
 
 class AppWeatherRepositoryTest {
 
+    private lateinit var SUT: AppWeatherRepository
+    private lateinit var weatherSrcMock: WeatherDataSource
+    private lateinit var configSrcMock: ConfigurationDataSource
+    private lateinit var weatherEntityMock: WeatherEntity
+    private lateinit var forecastListMock: MutableList<ForecastEntity>
+    private lateinit var configurationMock: Configuration
+
+    @Before
+    fun setup() {
+        weatherSrcMock = mock()
+        configSrcMock = mock()
+        configurationMock = Configuration(Units.SI, System.currentTimeMillis())
+        whenever(configSrcMock.getConfiguration()).thenReturn(configurationMock)
+        weatherEntityMock = WeatherEntity(
+                1,
+                "",
+                "",
+                1.0,
+                1.0,
+                1.0,
+                Units.IMPERIAL,
+                "MX",
+                Coordinates(1.1, -1.0),
+                null
+        )
+        forecastListMock = mutableListOf(
+                ForecastEntity(
+                        1,
+                        DateTime(),
+                        1.1,
+                        "",
+                        "", 1.0, -1.0)
+        )
+        SUT = AppWeatherRepository(weatherSrcMock, configSrcMock)
+    }
+
     @Test
     fun getCurrentWeather() {
+        runBlocking {
+            try {
+                weatherSrcLocalEmpty()
+                notConnected()
+                SUT.getCurrentWeather(Coordinates(0.0, 0.0))
+                verify(weatherSrcMock, times(1)).getCurrentWeatherFromLocal(any())
+                verify(configSrcMock, times(1)).getConfiguration()
+                verify(configSrcMock, times(1)).getNetworkStatus()
+            } catch (exception: Exception) {
+                assert(exception is ConnectivityException)
+            }
+            // TODO: Case when the time difference between the last update is greater than the threshold.
+            // In that case, the repository should only check of internet connection
+            // If there is internet connection then the repository gets the most recent
+            // weather for the "current" location.
+            // Then it must be saved to the local.
+            // TODO: Case when at the end of the function the saved weather is returned.
+        }
     }
 
     @Test
     fun get5DayCurrentForecast() {
+        // TODO
+    }
+
+    private fun notConnected() {
+        whenever(configSrcMock.getNetworkStatus()).thenReturn(NetworkStatus.NOT_CONNECTED)
+    }
+
+    private fun connected() {
+        whenever(configSrcMock.getNetworkStatus()).thenReturn(NetworkStatus.CONNECTED)
+    }
+
+    private suspend fun weatherSrcLocalEmpty() {
+        whenever(weatherSrcMock.getCurrentWeatherFromLocal(any())).thenReturn(null)
+    }
+
+    private suspend fun weatherSrcErrorFromService(exceptionClass: KClass<out Throwable>) {
+        whenever(weatherSrcMock.getCurrentWeatherFromService(any(), any()))
+                .thenThrow(createInstance(exceptionClass))
+    }
+
+    private suspend fun weatherSrcSuccessFromService() {
+        whenever(weatherSrcMock.getCurrentWeatherFromService(any(), any()))
+                .thenReturn(weatherEntityMock)
     }
 }
