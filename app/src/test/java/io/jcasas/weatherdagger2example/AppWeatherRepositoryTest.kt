c6afc63df739079602399e7e53c4fbc334ce6da3
Juan Casas <imhouses@ciencias.unam.mx>
2020-02-01 14:15:42 -0600

    Added: Test for Weather Repository.

---
+++ app/src/test/java/io/jcasas/weatherdagger2example/AppWeatherRepositoryTest.kt
@@ -0,0 +1,16 @@
+package io.jcasas.weatherdagger2example
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class AppWeatherRepositoryTest {
+
+    @Test
+    fun getCurrentWeather() {
+    }
+
+    @Test
+    fun get5DayCurrentForecast() {
+    }
+}
