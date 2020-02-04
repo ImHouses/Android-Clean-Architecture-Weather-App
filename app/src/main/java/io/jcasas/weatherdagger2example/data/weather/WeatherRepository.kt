cb277c7eff07663b7e7b79e2b131a3f48e881209
Juan Casas <imhouses@ciencias.unam.mx>
2020-02-03 21:03:59 -0600

    Changed-Added: TODO for not using two functions for Weather and Forecast

--- app/src/main/java/io/jcasas/weatherdagger2example/data/weather/WeatherRepository.kt
+++ app/src/main/java/io/jcasas/weatherdagger2example/data/weather/WeatherRepository.kt
@@ -6,6 +6,8 @@ import io.jcasas.weatherdagger2example.domain.weather.WeatherEntity
 
 interface WeatherRepository {
 
+    // TODO: Create an entity that contains both WeatherEntity and the List of the ForecastEntity.
+
     suspend fun getCurrentWeather(coordinates: Coordinates): WeatherEntity
 
     suspend fun get5DayCurrentForecast(coordinates: Coordinates): List<ForecastEntity>
