c7cc02ec61c01890668b7f02cf17603cc60358da
Juan Casas <imhouses@ciencias.unam.mx>
2020-02-06 22:50:01 -0600

    Changed: Added functions for hiding views.

--- app/src/main/java/io/jcasas/weatherdagger2example/util/Extensions.kt
+++ app/src/main/java/io/jcasas/weatherdagger2example/util/Extensions.kt
@@ -1,5 +1,7 @@
 package io.jcasas.weatherdagger2example.util
 
+import android.view.View
+import android.widget.ProgressBar
 import io.jcasas.weatherdagger2example.domain.ErrorHandler
 
 suspend inline fun <T> tryOrHandle(errorHandler: ErrorHandler, body: ()->T): Resource<T> = try {
@@ -7,4 +9,12 @@ suspend inline fun <T> tryOrHandle(errorHandler: ErrorHandler, body: ()->T): Res
 } catch (throwable: Throwable) {
     throwable.printStackTrace()
     Resource.Error(errorHandler.getError(throwable))
+}
+
+fun ProgressBar.hide() {
+    visibility = View.GONE
+}
+
+fun ProgressBar.show() {
+    visibility = View.VISIBLE
 }
